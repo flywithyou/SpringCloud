8fea29e6b5a53dc818b6b22e3159425b8996490b
a1121323395 <57247746+a1121323395@users.noreply.github.com>
2019-11-06 14:56:57 +0800

    sp04-OrderService

---
+++ sp04-orderservice/src/main/java/com/tedu/sp04/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.sp04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringCloudApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
